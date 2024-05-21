5269de1a72f462a2da879923de617e60e9bd7e98
ylem76 <ylem76@gmail.com>
2024-05-21 23:57:14 +0900

    style: auth.js add comment

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,6 +1,30 @@
 'use strict';
 
+/**
+ *
+ * 폼과 버튼 클래스 이름을 이용해 formValidation 함수 실행
+ * 폼 내부의 input리스트를 가져와 inputState 생성 -> 이 객체를 이용해 폼 전체의 인풋이 조건을 충족하는지 확인
+ * focusout했을 때 해당 input validation 실행 -> 오류 등을 inputState업데이트 -> 전체 인풋 validation체크
+ *
+ */
+
 const formValidation = (formClassName, submitButtonClassName) => {
+	// elements
+	const wrapperEl = document.querySelector('.' + formClassName);
+	const btnEl = document.querySelector('.' + submitButtonClassName);
+	const inputList = wrapperEl.querySelectorAll('input');
+
+	// init inputState
+	const initInputState = (inputList) => {
+		const inputState = {};
+		inputList.forEach((input) => {
+			const id = input.id;
+			inputState[id] = undefined;
+		});
+		return inputState;
+	};
+	const inputState = initInputState(inputList);
+
 	// validation
 	const inputValidation = (type, value, targetPWValue) => {
 		let errorMessage = '';
@@ -37,26 +61,31 @@ const formValidation = (formClassName, submitButtonClassName) => {
 				}
 
 			default:
-				// 처리할 내용이 없는 경우
 				break;
 		}
 
 		return errorMessage;
 	};
 
+	// render error message
 	const renderErrMsg = (inputId, errorMessage) => {
 		const errMsgElement = document.getElementById(`${inputId}Err`);
 		const inputEl = document.getElementById(inputId);
+
+		// error class toggle
 		if (errorMessage) {
 			inputEl.parentElement.classList.add('error');
 		} else {
 			inputEl.parentElement.classList.remove('error');
 		}
 
+		// error message change
 		if (errMsgElement) {
 			errMsgElement.textContent = errorMessage;
 		}
 	};
+
+	// all input valid check
 	const isAllInputsValid = (inputState) => {
 		for (const inputId in inputState) {
 			if (inputState[inputId] !== '') {
@@ -66,17 +95,9 @@ const formValidation = (formClassName, submitButtonClassName) => {
 		return true;
 	};
 
-	// focus out
-	const wrapperEl = document.getElementsByClassName(formClassName)[0];
-	const btnEl = document.querySelector('.' + submitButtonClassName);
-	const inputList = wrapperEl.querySelectorAll('input');
-	const inputState = {};
-	inputList.forEach((input) => {
-		const id = input.id;
-		inputState[id] = undefined;
-	});
-
-	wrapperEl.addEventListener('focusout', (e) => {
+	// handlers
+	const handleInputFocusout = (e) => {
+		// validation
 		const currentEl = e.target;
 		if (currentEl.tagName !== 'INPUT') {
 			return;
@@ -92,10 +113,9 @@ const formValidation = (formClassName, submitButtonClassName) => {
 		const isAllValid = isAllInputsValid(inputState);
 
 		btnEl.disabled = !isAllValid;
-	});
+	};
 
-	wrapperEl.addEventListener('click', (e) => {
-		// handle visibility button
+	const handlePWVisibility = (e) => {
 		if (e.target.tagName === 'BUTTON') {
 			const inputEl = e.target.closest('.input_wrapper').querySelector('input');
 			const inputType = inputEl.getAttribute('type');
@@ -107,15 +127,17 @@ const formValidation = (formClassName, submitButtonClassName) => {
 			}
 			e.target.classList.toggle('close');
 		}
-		if (e.target.classList.contains('btn_visibility')) {
-		}
-	});
+	};
 
-	btnEl.addEventListener('click', () => {
-		// 페이지 이동
+	const handleSubmitClick = () => {
+		// page move
 		const address = btnEl.getAttribute('data-href');
 		window.location.href = address;
-	});
+	};
+
+	wrapperEl.addEventListener('focusout', handleInputFocusout);
+	wrapperEl.addEventListener('click', handlePWVisibility);
+	btnEl.addEventListener('click', handleSubmitClick);
 };
 
 formValidation('formWrapper', 'btnSubmit');
