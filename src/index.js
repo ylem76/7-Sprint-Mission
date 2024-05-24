222b36310c84e5789066502c5f216c4cc14d7fb7
ylem76 <ylem76@gmail.com>
2024-05-24 11:09:18 +0900

    refactor: modify react createRoot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import ReactDOM from 'react-dom';
 import Main from './Main';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Main tab='home' />);
