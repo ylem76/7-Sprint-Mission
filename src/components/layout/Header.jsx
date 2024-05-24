dc0a11bcf4da55b8b16b8bdad732050121a1c577
ylem76 <ylem76@gmail.com>
2024-05-24 23:46:59 +0900

    add header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,3 +1,31 @@
+import logoImg from '../../assets/images/logo.png';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
-  return <div>Header</div>;
+  const pathName = window.location.pathname;
+  const currentPathName = pathName.match(/\/([^\/]+)(\/|$)/)[1];
+
+  return (
+    <header className={styles.gnb}>
+      <div className={styles.logo}>
+        <Link to='/'>
+          <img src={logoImg} alt='판다마켓 로고' />
+        </Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>자유게시판</Link>
+          </li>
+          <li>
+            <Link to='/items' className={currentPathName === 'items' ? 'current' : ''}>
+              중고마켓
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Link to='/login'>로그인</Link>
+    </header>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Main from './Main';
 import { createRoot } from 'react-dom/client';
+import './assets/css/reset.css';
+import './assets/css/globals.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
