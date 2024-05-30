14a767b12b6d4efa857d6c59f1eed115098bdaac
ylem76 <ylem76@gmail.com>
2024-05-31 00:26:24 +0900

    style: change input name

--- src/pages/AddItemPage.js
+++ src/pages/AddItemPage.js
@@ -8,19 +8,17 @@ export default function AddItemPage() {
           <h1>상품 등록하기</h1>
           <button type='submit'>등록</button>
         </div>
-        <label htmlFor='ipt-file'>상품 이미지</label>
-        <input id='ipt-file' type='file' name='product-image' />
+        <label htmlFor='ipt-files'>상품 이미지</label>
+        <input id='ipt-files' type='file' name='images' />
 
         <label htmlFor='ipt-product-name'>상품명</label>
-        <input type='text' id='ipt-product-name' name='product-name' />
+        <input type='text' id='ipt-product-name' name='name' />
 
         <label htmlFor='ipt-product-description'>상품 소개</label>
-        <textarea
-          name='product-description'
-          id='ipt-product-description'></textarea>
+        <textarea name='description' id='ipt-product-description'></textarea>
 
         <label htmlFor='ipt-product-price'>판매 가격</label>
-        <input name='product-price' id='ipt-product-price' type='number' />
+        <input name='price' id='ipt-product-price' type='number' />
 
         <label htmlFor='ipt-tag'>태그</label>
         <input type='text' name='tag' id='ipt-tag' />
