6294ef87df81a05b8a08da72fdac0a3f9ed67887
ylem76 <ylem76@gmail.com>
2024-06-28 18:47:27 +0900

    refactor: buttons 타입 추가

--- src/components/ui/Buttons.tsx
+++ src/components/ui/Buttons.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import ico_plus from '../../assets/images/ico_plus.svg';
 import ico_x from '../../assets/images/ico_x.svg';
 
-export function ButtonRounded({ className, children, ...rest }) {
+interface Props {
+  className?: String;
+  children?: ReactNode;
+}
+export function ButtonRounded({ className, children, ...rest }: Props) {
   return (
     <button
       className='flex justify-center items-center bg-blue-500 text-white font-semibold text-base leading-none border-0 cursor-pointer rounded-full'
@@ -11,7 +15,7 @@ export function ButtonRounded({ className, children, ...rest }) {
     </button>
   );
 }
-export function Button({ className, children, ...rest }) {
+export function Button({ className, children, ...rest }: Props) {
   return (
     <button
       className='w-32 h-12 flex justify-center items-center bg-blue-500 text-white font-semibold text-base leading-none border-0 cursor-pointer rounded-md'
@@ -21,7 +25,7 @@ export function Button({ className, children, ...rest }) {
   );
 }
 
-export function ButtonAddImage({ children, ...rest }) {
+export function ButtonAddImage({ className, children, ...rest }: Props) {
   return (
     <button
       className={`w-42 h-42 flex flex-col justify-center items-center bg-gray-100 rounded-md`}
@@ -35,7 +39,7 @@ export function ButtonAddImage({ children, ...rest }) {
   );
 }
 
-export function ButtonDeleteRound({ className, children, ...rest }) {
+export function ButtonDeleteRound({ className, children, ...rest }: Props) {
   return (
     <button
       className={`w-5 h-5 flex justify-center items-center bg-gray-100 rounded-full bg-gray-400 ${className}`}
